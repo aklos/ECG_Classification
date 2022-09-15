813b05f151d8319b23a780053f320842dbe5999b
Alex Klos <alexklos@prohobo.dev>
2022-09-15 22:19:50 +0200

    pass chunked data to model

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main(edf_path: str, offset: int = 0, limit: int = 0):
     list_chunked = [data[i:i + chunk_size]
                     for i in range(0, len(data), chunk_size)]
 
-    results = predict_labels([data], 500, ['ecg'])
+    results = predict_labels([list_chunked], 500, ['ecg'])
     print(results)
 
 
